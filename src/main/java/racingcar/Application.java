eea4d487c777cfbb1fcfa37bf17b2cff1661583d
taemin kim <world32033@gmail.com>
2025-10-26 22:45:07 +0900

    refactor: Application에서 Controller 주입 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,20 +1,18 @@
 package racingcar;
 
-
-import camp.nextstep.edu.missionutils.Console;
-
+import racingcar.controller.GameController;
+import racingcar.model.ThresholdMoveStrategy;
+import racingcar.model.RandomNumberGenerator;
+import racingcar.View.ConsoleView;
 
 public class Application {
     public static void main(String[] args) {
 
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String namesInput = Console.readLine();
-
-
-        System.out.println("시도할 횟수는 몇 회인가요?");
-        String attemptsInput = Console.readLine();
-
-        System.out.println("\n실행 결과");
-
+        GameController controller = new GameController(
+                new ConsoleView(),
+                new ThresholdMoveStrategy(),
+                new RandomNumberGenerator()
+        );
+        controller.run();
     }
 }
