242d70be3e96b453384a562fa5381eb6d0720d58
taemin kim <world32033@gmail.com>
2025-10-26 22:26:02 +0900

    feat: ResultFormatter 구현

---
+++ src/main/java/racingcar/View/ResultFormatter.java
@@ -0,0 +1,27 @@
+package racingcar.View;
+import java.util.List;
+import java.util.stream.Collectors;
+import racingcar.model.Car;
+import racingcar.model.Cars;
+
+public final class ResultFormatter {
+    private ResultFormatter() {}
+
+
+    public static String formatRound(Cars cars) {
+        return cars.asList().stream()
+                .map(ResultFormatter::formatOne)
+                .collect(Collectors.joining(System.lineSeparator()))
+                .trim();
+    }
+
+
+    private static String formatOne(Car c) {
+        return c.name() + " : " + "-".repeat(c.position());
+    }
+
+
+    public static String formatWinners(List<String> winners) {
+        return "최종 우승자 : " + String.join(", ", winners);
+    }
+}
